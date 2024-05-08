6eb5a03668ff4178e0e27f2892271cc7c042fed7
Jamie Zhao <rainyday.tea.party@gmail.com>
2024-05-08 18:55:31 -0400

    Add scuffed latex rendering support

---
+++ commands/utility/latex.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const katex = require('katex');
+const puppeteer = require('puppeteer');
+const { SlashCommandBuilder } = require('discord.js');
+
+// Global options for KaTeX rendering, contains some user safeguards
+const katexOptions = {
+    displayMode: true,
+    output: 'mathml',
+    maxSize: 50,
+    maxExpand: 200,
+    macros: {
+        // Number sets
+        "\\R": "\\mathbb{R}",
+        "\\N": "\\mathbb{N}",
+        "\\Z": "\\mathbb{Z}",
+        "\\Q": "\\mathbb{Q}",
+        "\\C": "\\mathbb{C}",
+    }
+}
+
+var browser, page;
+var htmlTemplate = fs.readFileSync('./commands/utility/latex-template.html', 'utf8');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('latex')
+        .setDescription('Render LaTeX code into an image')
+        .addStringOption(option =>
+            option.setName('code')
+                .setDescription('The LaTeX code to render')
+                .setRequired(true)),
+    async execute(interaction) {
+        const tex = interaction.options.getString('code');
+        try {
+            let htmlString = katex.renderToString(tex, katexOptions);
+            await interaction.deferReply({
+                ephemeral: true
+            });
+            if (browser === undefined) {
+                browser = await puppeteer.launch();
+                page = await browser.newPage();
+            }
+            await page.setContent(htmlTemplate.replace('PLACEHOLDER', htmlString));
+            console.log(htmlString);
+            let elem = await page.$('.katex');
+            let image = await elem.screenshot({type: 'png'});
+            await interaction.editReply({ 
+                files: [{
+                    attachment: image
+                }] 
+            });
+        } catch (err) {
+            await interaction.editReply(`An error occurred while rendering the expression.`);
+            console.log(`Error while rendering LaTeX: ${err}`);
+        }
+    }
+};
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// Load environment variables
 require('dotenv').config();
 
-const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+
+
+// ================= [[ Imports ]] =================
+
+const fs = require('fs');
+const path = require('path');
+const { Client, Collection, 
+    Events, GatewayIntentBits } = require('discord.js');
+
 const GUILD_ID = '1087532733288415343';
 const LOG_CHANNEL_ID = '1203852989614260274';
 const LOG_FORMAT = 'Message deleted from `%s` in #`%s`: %s';
@@ -13,13 +22,51 @@ const client = new Client({
     ]
 });
 
+
+// ================= [[ Initialization ]] =================
+
+console.log('Starting bot...');
+
 client.commands = new Collection();
 
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+console.log('Loading commands...');
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+            console.log(`\tCommand "${command.data.name}" registered.`);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+console.log('Logging in to Discord...');
+
+// Code below should finish before the bot logs in
+setTimeout(() => {
+    client.login(process.env.BOT_TOKEN);
+}, 1000);
+
+// =============== [[ Event Handlers ]] ===============
+
+// .once for one-time events
 client.once(Events.ClientReady, async (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    console.log(`Success! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.MessageDelete, async (msg) => {
+
+    // Reject bot messages
     if (msg.author.bot) {
         return;
     }
@@ -29,9 +76,13 @@ client.on(Events.MessageDelete, async (msg) => {
     if (msg.guild.id === GUILD_ID) {
         let logChannel = await client.channels.fetch(LOG_CHANNEL_ID);
         let msgBuffer = LOG_FORMAT.replace('%s', author).replace('%s', channel.name).replace('%s', content);
+
+        // Truncate message if it's too long
         if (msgBuffer.length > 2000) {
             msgBuffer = msgBuffer.slice(0, 1997) + '...';
         }
+        
+        // Catch anyways for safety
         logChannel.send(msgBuffer)
             .catch(console.error);
     }
@@ -45,4 +96,25 @@ client.on(Events.MessageCreate, async (msg) => {
     console.log(`Message received in '${msg.guild.name}' #${msg.channel.name} from ${msg.author.tag}: ${msg.content}`);
 });
 
-client.login(process.env.BOT_TOKEN);
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
