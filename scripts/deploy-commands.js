e583039b27ab120a73503aa4b54c6bef5ecc9290
Jamie Zhao <rainyday.tea.party@gmail.com>
2024-05-12 21:34:40 -0400

    Improved deploy-command

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -5,20 +5,37 @@
  */
 require('dotenv').config();
 
-const { REST, Routes, Client } = require('discord.js');
-const { clientId, guildId, token } = {
-	clientId: process.env.CLIENT_ID,
-	guildId: process.env.GUILD_ID,
-	token: process.env.BOT_TOKEN,
-};
 const fs = require('node:fs');
 const path = require('node:path');
+const { REST, Routes, Client, GatewayIntentBits } = require('discord.js');
+const clientId = process.env.CLIENT_ID;
+const token = process.env.BOT_TOKEN;
+var guildId = '-1';
+var remove = false;
 
+const HELP_MESSAGE = `Usage: npm run deploy-commands -- [--remove] --guildid <guild_id> | --all \n
+--all                : Deploy commands to all known guilds.
+--guildid <guild_id> : Deploy to the guild with <guild_id>.
+--remove             : Remove all commands from the target guild(s) instead of deploying them.
+`;
 
 // Parse arguments
+if (process.argv.includes('-h') || process.argv.includes('--help')) {
+	console.log(HELP_MESSAGE);
+	process.exit(0);
+}
+if (process.argv.includes('--all')) {
+	guildId = 'all';
+	console.log('Deploying commands to all known guilds...');
+} else if (process.argv.includes('--guildid')) {
+	guildId = process.argv[process.argv.indexOf('--guildid') + 1];
+}
+if (process.argv.includes('--remove')) {
+	remove = true;
+}
 
 
-// Collect command data
+// Collect command data as JSON objects
 const commands = [];
 const foldersPath = path.resolve(__dirname, '../commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -42,17 +59,36 @@ const rest = new REST().setToken(token);
 
 (async () => {
 	try {
-		let client = new Client();
+		let client = new Client({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages
+			]
+		});
 		await client.login(token);
+		if (remove) console.log('Executing in remove mode.');
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		if (guildId === 'all') {
+			const guilds = await client.guilds.fetch();
+			for (const guild of guilds.values()) {
+				const data = await rest.put(
+					Routes.applicationGuildCommands(clientId, guild.id),
+					{ body: remove? {} : commands },
+				);
+				console.log(`Successfully reloaded ${data.length} application (/) commands for guild ${guild.id}.`);
+			}
+			process.exit(0);
+		} else {
+			// If a specific guild is targeted, only update the commands in that guild
+			const data = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: remove? {} : commands },
+			);
+			console.log(`Successfully reloaded ${data.length} application (/) commands for guild ${guildId}.`);
+			process.exit(0);
+		}
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
